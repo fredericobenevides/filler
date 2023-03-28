3daa9abf7314fa308d4bddeb31a85b17b5196198
Frederico Benevides <frederico@benevides.io>
2023-03-28 17:54:52 -0300

    Create the task fillers to list all the fillers from a path

--- src/filler/utils.clj
+++ src/filler/utils.clj
@@ -1,8 +1,13 @@
 (ns filler.utils
-  (:require [babashka.fs :as fs]))
+  (:require [babashka.fs :as fs]
+            [clojure.edn :as edn]))
 
 (def CONFIG-LOCATION (.toString (fs/expand-home (str "~/" ".filler.edn"))))
 
 (defn create-config [path]
   (fs/create-file CONFIG-LOCATION)
   (spit CONFIG-LOCATION {:path path}))
+
+(defn read-config-file [path]
+  (edn/read-string (slurp path)))
+
